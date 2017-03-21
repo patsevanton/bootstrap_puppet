3572327e37ce9e74b1090d5e155ed453de6156e8
Ingar Smedstad <ingar.smedstad@uib.no>
2017-03-21 15:30:01 +0100

    adding xome checks and full path for puppet

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,16 @@
 # Install puppet from puppetlabs
 case $(lsb_release -si) in
     Ubuntu)
-        apt list --installed 2>/dev/null |grep -q uib-puppetnode && apt-get purge uib-puppetnode
+        apt list --installed 2>/dev/null |grep -q uib-puppetnode && apt-get purge -y uib-puppetnode
         wget -P /tmp https://apt.puppetlabs.com/puppetlabs-release-pc1-$(lsb_release -sc).deb
         dpkg -i /tmp/puppetlabs-release-pc1-$(lsb_release -sc).deb
         apt update
-        apt install puppet-agent
+        apt list --installed 2>/dev/null |grep -q puppet-agent ||apt install -y puppet-agent
         ;;
     CentOS|RedHat)
-        yum list installed |grep -q uib-puppetnode && yum erase uib-puppetnode
+        yum list installed |grep -q uib-puppetnode && yum erase -y uib-puppetnode
         rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-$(lsb_release -sr|cut -d'.' -f1).noarch.rpm
-        yum install puppet-agent
+        yum list installed |grep -q puppet-agent || yum install -y puppet-agent
         ;;
     *)
         echo "$(lsb_release -si) is not supported."
@@ -23,6 +23,6 @@ esac
 #mkdir -p /root/bootstrap/modules
 #puppet module install --modulepath=/root/bootstrap/modules puppetlabs/puppet_agent
 
-puppet agent -t --server client-dev.puppet.uib.no --waitforcert 60 --masterport 8140 --report true
+/opt/puppetlabs/bin/puppet agent -t --server client-dev.puppet.uib.no --waitforcert 60 --masterport 8140 --report true
 
 exit 0
